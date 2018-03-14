8f511dcc9ddd9aacaa650828d3209a27a0678250
axle12693 <axle12693@gmail.com>
2018-03-14 12:43:22 -0600

    Fixed "all zeroes" error
    Now works if there is a datapoint with all zeros.

--- Network.py
+++ Network.py
@@ -10,7 +10,7 @@ class Network:
         self.layers = []
         for index, layer in enumerate(topology):
             self.layers.append([])
-            for neuron in range(layer[0]):
+            for neuron in range(layer):
                 self.layers[index].append(Neuron())
         self.num_inputs = num_inputs
         self.__init_weights__()
@@ -22,10 +22,10 @@ class Network:
             for j in self.layers[i]:
                 if i == 0:
                     for net_input in range(self.num_inputs):
-                        j.weights.append(random.uniform(-1.0, 1.0))
+                        j.weights.append([random.uniform(-1.0, 1.0), random.uniform(-1.0, 1.0)])
                 else:
                     for neuron in range(len(self.layers[i - 1])):
-                        j.weights.append(random.uniform(-1.0, 1.0))
+                        j.weights.append([random.uniform(-1.0, 1.0), random.uniform(-1.0, 1.0)])
 
     def run_network(self, inputs):
         """Runs the network through a SINGLE set of inputs (one data point) and returns the result. No training."""
@@ -33,12 +33,12 @@ class Network:
             if i == 0:
                 for j in self.layers[i]:
                     for index, net_input in enumerate(inputs):
-                        j.enqueue(net_input * j.weights[index])
+                        j.enqueue(net_input * j.weights[index][0] + j.weights[index][1])
             else:
                 for index, neuron in enumerate(self.layers[i - 1]):
                     neuron.calc_output()
                     for j in self.layers[i]:
-                        j.enqueue(neuron.output * j.weights[index])
+                        j.enqueue(neuron.output * j.weights[index][0] + j.weights[index][1])
         output = []
         for out_neuron in self.layers[len(self.layers) - 1]:
             out_neuron.calc_output()
@@ -51,57 +51,37 @@ class Network:
         """Runs the network over multiple data points, and iterates one generation of a semi-genetic algorithm."""
         output = []
         for data_index, data_point in enumerate(inputs):  # For each set of inputs
-            # for layer in range(len(self.layers)):
-            #     if layer == 0:  # if it's the first layer
-            #         for neuron in self.layers[layer]:
-            #             for index, net_input in enumerate(inputs):
-            #                 neuron.enqueue(inputs[data_index][index] * neuron.weights[index])
-            #     else:  # if it's not the first layer
-            #         for neuron in self.layers[layer]:
-            #             for index, prev_neuron in enumerate(self.layers[layer - 1]):
-            #                 neuron.enqueue(prev_neuron.output * neuron.weights[index])
-            #     for neuron in self.layers[layer]:
-            #         neuron.calc_output()
-            # output.append([])
-            # for out_neuron in self.layers[len(self.layers) - 1]:
-            #     output[data_index].append(out_neuron.output)
             output.append(self.run_network(inputs[data_index]))
 
         # Error calculated this way so that larger individual errors impact the end result more
         original_error = 0
+        original_max_error = 0
         for index1, data_point in enumerate(output):
             for index2, out in enumerate(data_point):
                 original_error += (out - expected[index1][index2]) ** 2.0
-                # print("Expected: " + str(expected[index1]) + ", got: " + str(data_point))
+                if (out - expected[index1][index2]) ** 2 > original_max_error:
+                    original_max_error = (out - expected[index1][index2]) ** 2
 
         original_error = sqrt(original_error)
+        original_max_error = sqrt(original_max_error)
         original_weights = self.__get_weights__()
 
-        for i in range(100):
-            for j in range(random.randint(1, 100)):
+        for i in range(1000):
+            if i % 100 == 0:
+                print("Starting the " + str(i) + "th run.")
+            for j in range(random.randint(1, 1000)):
                 l_index = random.randint(0, len(self.layers) - 1)
                 n_index = random.randint(0, len(self.layers[l_index]) - 1)
                 w_index = random.randint(0, len(self.layers[l_index][n_index].weights) - 1)
-                w_delta = self.temperature * random.uniform(-10, 10)
+                w_delta = self.temperature * random.uniform(-1, 1)
+                index = random.randint(0, 1)
                 # self.layers[l_index][n_index].weights[w_index] = \
                 #     max(-1.0, min(self.layers[l_index][n_index].weights[w_index] + w_delta, 1.0))
-                self.layers[l_index][n_index].weights[w_index] = \
-                    self.layers[l_index][n_index].weights[w_index] + w_delta
+                self.layers[l_index][n_index].weights[w_index][index] = \
+                    self.layers[l_index][n_index].weights[w_index][index] + w_delta
 
             output = []
             for data_index, data_point in enumerate(inputs):  # For each set of inputs
-                # for layer in range(len(self.layers)):
-                #     if layer == 0:  # if it's the first layer
-                #         for neuron in self.layers[layer]:
-                #             for index, net_input in enumerate(inputs):
-                #                 neuron.enqueue(inputs[data_index][index] * neuron.weights[index])
-                #     else:  # if it's not the first layer
-                #         for neuron in self.layers[layer]:
-                #             for index, prev_neuron in enumerate(self.layers[layer - 1]):
-                #                 neuron.enqueue(prev_neuron.output * neuron.weights[index])
-                # output.append([])
-                # for out_neuron in self.layers[len(self.layers) - 1]:
-                #     output[data_index].append(out_neuron.output)
                 output.append(self.run_network(inputs[data_index]))
 
             # Error calculated this way so that larger individual errors impact the end result more
@@ -122,7 +102,7 @@ class Network:
         if not changed:
             self.temperature *= 0.99
             print("Setting temperature to " + str(self.temperature))
-        return original_error
+        return [original_error, original_max_error]
 
     def __get_weights__(self):
         weights = []
@@ -131,11 +111,15 @@ class Network:
             for n_index, neuron in enumerate(self.layers[l_index]):
                 weights[l_index].append([])
                 for w_index, weight in enumerate(self.layers[l_index][n_index].weights):
-                    weights[l_index][n_index].append(self.layers[l_index][n_index].weights[w_index])
+                    weights[l_index][n_index].append([])
+                    for index, weight_type in enumerate(weight):
+                        weights[l_index][n_index][w_index].append(weight_type)
         return weights
 
     def __set_weights__(self, weights):
         for l_index, layer in enumerate(self.layers):
             for n_index, neuron in enumerate(self.layers[l_index]):
                 for w_index, weight in enumerate(self.layers[l_index][n_index].weights):
-                    self.layers[l_index][n_index].weights[w_index] = weights[l_index][n_index][w_index]
+                    for index, weight_type in enumerate(weight):
+                        self.layers[l_index][n_index].weights[w_index][index] = \
+                            weights[l_index][n_index][w_index][index]
--- test_driver.py
+++ test_driver.py
@@ -1,12 +1,12 @@
 from Network import Network
 
 print("Creating network.")
-net = Network([[11], [11], [11], [11], [11], [11], [11], [11], [11], [11], [4]], 2)
+net = Network([4, 8, 16, 32, 16, 8, 4], 2)
 training_data = [[[0, 0], [0, 1], [1, 0], [1, 1]], [[0, 0, 0, 1], [0, 1, 1, 1], [0, 1, 1, 1], [1, 1, 0, 1]]]
 print("Training network...")
 current_error = net.train_network(training_data[0], training_data[1])
 print("Current network error: " + str(current_error))
-while current_error > 0.001 * len(training_data[1]) * len(training_data[1][0]):
+while current_error[1] > 0.2:  # * len(training_data[1]) * len(training_data[1][0]):
     print()
     print("Training network...")
     current_error = net.train_network(training_data[0], training_data[1])
